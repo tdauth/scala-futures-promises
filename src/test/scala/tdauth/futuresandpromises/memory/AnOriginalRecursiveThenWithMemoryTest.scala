174f6702fce1a443d4cd62d507d2d7f4d139e664
Tamino Dauth <tamino@cdauth.eu>
2018-10-04 15:39:20 +0200

    Add original memory exhaustion test

---
+++ src/test/scala/tdauth/futuresandpromises/memory/AnOriginalRecursiveThenWithMemoryTest.scala
@@ -0,0 +1,38 @@
+package tdauth.futuresandpromises.memory
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+
+import tdauth.futuresandpromises.AbstractUnitSpec
+
+/**
+ * This test uses recursive calls to Future.flatMap to create arrays whose
+ *  combined size is slightly greater than the JVM heap size. A previous
+ *  implementation of Future.flatMap would retain references to each array,
+ *  resulting in a speedy OutOfMemoryError. Now, each array should be freed soon
+ *  after it is created and the test should complete without problems.
+ */
+class AnOriginalRecursiveThenWithMemoryTest extends AbstractUnitSpec {
+  getTestName should "call a function asynchronously" in {
+    def loop(i: Int, arraySize: Int): Future[Unit] = {
+      val array = new Array[Byte](arraySize)
+      Future.successful(i).flatMap { i =>
+        if (i == 0) {
+          Future.successful(())
+        } else {
+          array.size // Force closure to refer to array
+          loop(i - 1, arraySize)
+        }
+
+      }
+    }
+
+    val arraySize = 1000000
+    val tooManyArrays = (Runtime.getRuntime().totalMemory() / arraySize).toInt + 1
+    Await.ready(loop(tooManyArrays, arraySize), Duration.Inf)
+  }
+
+  override def getTestName: String = "OriginalRecursiveThenWithMemoryTest"
+}
